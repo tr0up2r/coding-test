e07b711f70368a566a5ea127b2c0a8a2e4ee5c4e
tr0up2r <kmydtr@gmail.com>
2023-03-22 15:29:10 +0900

    [020] a future city (shortest path)

---
+++ shortest_path/020_a_future_city.py
@@ -0,0 +1,48 @@
+INF = int(1e9)
+n, m = map(int, input().split())  # n: 회사 수(노드 수), m: 경로 수(간선 수)
+
+# 연결된 2개의 회사는 양방향으로, 1만큼의 시간으로 이동 가능
+# 1 <= n, m <= 100로 범위가 매우 한정적, 그래서 floyd-warshall을 쓸 수 있음
+
+'''
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+
+4 2
+1 3
+2 4
+3 4
+'''
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for i in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][i]+graph[i][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print(-1)
+else:
+    print(distance)
