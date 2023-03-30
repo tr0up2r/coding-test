3780e8a542950ebefa50ab34186dc4dbb6694d96
tr0up2r <kmydtr@gmail.com>
2023-03-30 18:36:55 +0900

    [034] failure rate (sort, library) * https://school.programmers.co.kr/learn/courses/30/lessons/42889

---
+++ sort/034_failure_rate.py
@@ -0,0 +1,17 @@
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+
+    for i in range(1, N + 1):
+        count = stages.count(i)
+
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        answer.append((i, fail))
+        length -= count
+
+    answer = sorted(answer, key=lambda x: x[1], reverse=True)
+    return [x[0] for x in answer]
