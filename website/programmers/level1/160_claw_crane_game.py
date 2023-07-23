12ac61e00d3f5425cc5c79c582826662fad0cc06
tr0up2r <kmydtr@gmail.com>
2023-07-23 17:45:46 +0900

    [160] claw crane game (크레인 인형뽑기 게임) (programmers level 1) * https://school.programmers.co.kr/learn/courses/30/lessons/64061

---
+++ website/programmers/level1/160_claw_crane_game.py
@@ -0,0 +1,16 @@
+def solution(board, moves):
+    stack, answer = [], 0
+
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move - 1] != 0:
+                stack.append(board[i][move - 1])
+                board[i][move - 1] = 0
+
+                if len(stack) >= 2 and stack[-1] == stack[-2]:
+                    for _ in range(2):
+                        stack.pop()
+                    answer += 2
+                break
+
+    return answer
